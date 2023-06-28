c1307ebb2695948193ea7e40ecf9214c7332320c
LorenzoMegna24 <megnalorenzo0@gmail.com>
2023-06-28 16:53:14 +0200

    Milestone 1-2 completate

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
-// });x
+// });
+
+//http://127.0.0.1:8000/api/projects
+
+Route::get('/projects', [ProjectController::class, 'index']);
