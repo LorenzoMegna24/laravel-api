e5107958bd69b81d5624e1ce5024f3fe3654de23
LorenzoMegna24 <megnalorenzo0@gmail.com>
2023-07-03 16:47:08 +0200

    Filtragggio per tecnologia

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -12,16 +12,38 @@ class ProjectController extends Controller
         // $projects = Project::all();
 
 
-        if($request->has('type_id')){
-            $projects = Project::with(['type', 'technologies'])->where('type_id', $request->type_id)->paginate(4);
-        } else{
-            $projects = Project::with(['type', 'technologies'])->paginate(4);
+        // if($request->has('type_id')){
+        //     $projects = Project::with(['type', 'technologies'])->where('type_id', $request->type_id)->paginate(4);
+        // } else{
+        //     $projects = Project::with(['type', 'technologies'])->paginate(4);
+        // }
+
+        // return response()->json([
+        //         'success'=>true,
+        //         'projects'=>$projects
+        //     ]);
+
+        $query = Project::with(['type', 'technologies']);
+
+        if( $request->has('type_id') ){
+            $query->where('type_id', $request->type_id );
         }
 
+        if ($request->has('technologies_id')) {
+            $technologieId = explode(',', $request->technologies_id);
+            $query->whereHas('technologies', function($query) use ($technologieId){
+                $query->whereIn('id', $technologieId);
+            });
+        }
+
+        $projects = $query->paginate(4);
+
         return response()->json([
-                'success'=>true,
-                'projects'=>$projects
-            ]);
+            'success'=>true,
+            'projects'=>$projects
+        ]);
+
+
     }
 
     public function show($slug){
